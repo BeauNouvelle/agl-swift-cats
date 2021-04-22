9a7210d7dd62ec6460efe22a20e37875be3a9e0a
Beau Nouvelle <beau@getswifty.dev>
2021-04-23 01:24:29 +1000

    Setup tableview

--- SwiftCats/ViewController.swift
+++ SwiftCats/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    let tableView = UITableView()
+    var genders = [Gender]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        NetworkService().getOwners { (owners) in
-            print(owners)
+        
+        setupSubviews()
+        loadCats()
+    }
+    
+    private func setupSubviews() {
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+    private func loadCats() {
+        NetworkService().getOwners { [weak self] (owners) in
+            self?.genders = owners.catsOnlyGroupedByGender()
         }
     }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return genders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
 
